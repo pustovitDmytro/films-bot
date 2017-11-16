8a83e84d61b5d09285d67bb64e06a1f21d309113
dmytroPustovit <pustic@gmail.com>
2017-11-17 01:13:33 +0200

    simple aiohttp

--- bot.py
+++ bot.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
+from aiohttp import web
 
-bot = telebot.TeleBot(config.token)
+async def handler(request):
+	print(request.match_info)
+	return web.Response(text='text')
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+# async def wshandler(request):
+#     ws = web.WebSocketResponse()
+#     await ws.prepare(request)
+
+#     async for msg in ws:
+#         if msg.type == web.MsgType.text:
+#             await ws.send_str("Hello, {}".format(msg.data))
+#         elif msg.type == web.MsgType.binary:
+#             await ws.send_bytes(msg.data)
+#         elif msg.type == web.MsgType.close:
+#             break
+
+#     return ws
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+	app = web.Application()
+	app.router.add_get('/films', handler)
+	web.run_app(app)
