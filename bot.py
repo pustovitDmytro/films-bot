518b00a5b6016cda86ac3a141fe89063682ebaa1
dmytroPustovit <pustic@gmail.com>
2017-11-19 21:44:17 +0200

    echo with aiohttp

--- bot.py
+++ bot.py
@@ -2,27 +2,42 @@
 import config
 import telebot
 from aiohttp import web
+bot = telebot.TeleBot(config.token)
 
-async def handler(request):
-	print(request.method)
-	print(request.scheme)
-	return web.Response(text='text')
+# Process webhook calls
+async def handle(request):
+    print('receive', bot.token)
+    print(request.match_info.get('token'))
+    print('status',request.match_info.get('token') == bot.token)
+    if request.match_info.get('token') == bot.token:
+        print('true')
+        request_body_dict = await request.json()
+        print(request_body_dict)
+        update = telebot.types.Update.de_json(request_body_dict)
+        bot.process_new_updates([update])
+        return web.Response()
+    else:
+        return web.Response(status=403)
 
-# async def wshandler(request):
-#     ws = web.WebSocketResponse()
-#     await ws.prepare(request)
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
 
-#     async for msg in ws:
-#         if msg.type == web.MsgType.text:
-#             await ws.send_str("Hello, {}".format(msg.data))
-#         elif msg.type == web.MsgType.binary:
-#             await ws.send_bytes(msg.data)
-#         elif msg.type == web.MsgType.close:
-#             break
 
-#     return ws
+# Handle all other messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 if __name__ == '__main__':
 	app = web.Application()
-	app.router.add_get('/films', handler)
-	web.run_app(app)
+	app.router.add_post('/{token}/', handle)
+	print('film handler added')
+	web.run_app(
+    	app,
+    	host='0.0.0.0',
+    	port=3000,
+	)
