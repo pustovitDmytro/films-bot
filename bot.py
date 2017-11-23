df9a6553fac724d60bd225fbb08cee51bc8789b8
dmytroPustovit <pustic@gmail.com>
2017-11-24 01:27:44 +0200

    google search

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import config
 from aiohttp import web
 from aiohttp import ClientSession
+import json
 
 # Process webhook calls
 async def handle(request):
@@ -10,12 +11,29 @@ async def handle(request):
         request_body = await request.json()
         films = await getFilm(request_body['message']['text'])
         for film in films:
-            await postFilm(film)
-        return web.Response()
+            links = await getLinks(film['title'])
+            await postFilm(film, links)
+        return web.Response(body = json.dumps(links), status=200)
     else:
         return web.Response(status=403)
 
-
+async def getLinks(query):
+    mvstapeCX = 'partner-pub-7100147019647938:5181683182'
+    nnmclubCX = 'partner-pub-7768347321290299:4038489206'
+    result = {}
+    result['mvstape'] = await googleLink(mvstapeCX,query, 3)
+    result['nnmclub'] = await googleLink(nnmclubCX,query+' subs original')
+    return result
+async def googleLink(cx, query, n=5):
+    async with ClientSession() as session:
+        params = {'key': config.google,'cx':cx, 'q': query}
+        print(query)
+        async with session.get('https://www.googleapis.com/customsearch/v1', params=params) as resp:
+            body = await resp.json()
+            result = []
+            for i in range(min(n, int(body['queries']['request'][0]['totalResults']))):
+                result.append({"title": body['items'][i]['title'], "link": body['items'][i]['link']})
+            return result
 async def getFilm(text):
     async with ClientSession() as session:
         params = {'api_key': config.apikey, 'query': text}
@@ -23,7 +41,7 @@ async def getFilm(text):
             body = await resp.json()
             return body['results']
 
-async def postFilm(film):
+async def postFilm(film, links):
     data = {}
     data['chat_id'] = 238585617
     data['text'] = film['title']
@@ -33,7 +51,7 @@ async def telegram(method, data={}):
     async with ClientSession() as session:
         async with session.post('https://api.telegram.org/bot{}/{}'.format(config.token,method), data=data) as resp:
             return resp.json()
-            
+ 
 if __name__ == '__main__':
 	app = web.Application()
 	app.router.add_post('/{token}/', handle)
